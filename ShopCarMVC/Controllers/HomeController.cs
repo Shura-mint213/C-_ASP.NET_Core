261e5e0f7faf18e72e815c7b6113a90732269bb8
ShurikMint <myatnyys@list.ru>
2021-11-06 12:50:00 +0300

    Version 1.2

---
+++ ShopCarMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopCarMVC.Data.Interfaces;
+using ShopCarMVC.Data.Models;
+using ShopCarMVC.ViewModels;
+
+namespace ShopCarMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            this.carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                facCars = carRep.getFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
---
+++ ShopCarMVC/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopCarMVC.Data.Models;
+
+namespace ShopCarMVC.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> facCars { get; set; }
+    }
+}
